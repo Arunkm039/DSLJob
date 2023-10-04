f8eb192f806b0ef7716262b388eddf1d8d9ab281
Arunkm039 <57870537+Arunkm039@users.noreply.github.com>
2023-10-04 15:52:02 +0530

    Create hotspot.groovy

---
+++ hotspot.groovy
@@ -0,0 +1,41 @@
+def jobName = 'hotspot-worker'
+def jobDisplayName = 'Hotspot Worker'
+def repo = 'DevOpsTest'
+def includeBranchPattern = 'master feature/*'
+def org = 'Arunkm039'
+def credentials = 'Github'
+def jenkinsfilePath = 'Jenkinsfile'
+
+multibranchPipelineJob("hotspot/compute-engine/dev-qa/${jobName}") {
+
+    displayName("${jobDisplayName} DEV QA Build")
+
+    branchSources {
+
+        github {
+            id("${credentials}")
+            apiUri("https://github.developer.allianz.io/api/v3")
+            scanCredentialsId("${credentials}")
+            repoOwner("${org}")
+            repository("${repo}")
+            includes("${includeBranchPattern}")
+            buildOriginPRMerge(true)
+        }
+
+    }
+
+    description("Multibranch pipeline build for ${jobDisplayName}")
+
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath("${jenkinsfilePath}")
+        }
+    }
+
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(5)
+        }
+    }
+
+}
