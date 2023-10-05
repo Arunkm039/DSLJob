f6f75174ad1010127b37b6d0ca606632eb13a62f
Arunkm039 <57870537+Arunkm039@users.noreply.github.com>
2023-10-05 15:37:00 +0530

    Update hotspot.groovy

--- hotspot.groovy
+++ hotspot.groovy
@@ -6,42 +6,43 @@ def org = 'Arunkm039'
 def credentials = 'Github'
 def jenkinsfilePath = 'Jenkinsfile'
 
-pipelineJob("hotspot/compute-engine/dev-qa/${jobName}") {
+multibranchPipelineJob("hotspot/compute-engine/dev-qa/${jobName}") {
 
     displayName("${jobDisplayName} DEV QA Build")
 
-    parameters {
-		choiceParam {
-			name ("ENV")
-			description ("Choose target environment")
-			choices (['dev', 'qa', 'int'])
-		}
-		gitParameter {
-			name("GIT_BRANCH_TAG")
-			description("Available git branches")
-			type("PT_BRANCH_TAG")
-			defaultValue("")
-			branch("*")
-			branchFilter(".*")
-			tagFilter("*")
-			sortMode("DESCENDING_SMART")
-			selectedValue("DEFAULT")
-			useRepository("hotspot-backend-services.git")
-			quickFilterEnabled(true)
-			listSize("0")
-		}		
+    properties([
+    parameters([
+	    	choice(name: 'ENV', choices: ['dev', 'qa', 'int'], description: 'Choose target environment'),
+	    
+		gitParameter (
+			name: GIT_BRANCH_TAG,
+			description: Available git branches,
+			type: PT_BRANCH_TAG,
+			defaultValue: "",
+			branch: *,
+			branchFilter: .*,
+			tagFilter: *,
+			sortMode: DESCENDING_SMART,
+			selectedValue: DEFAULT,
+			useRepository: DevOpsTest.git,
+			quickFilterEnabled: true,
+			listSize: 0
+		),
 		
-		booleanParam {		
-			name("RUN_TESTS")
-			defaultValue(true)
-			description("Check to run tests")
-		}
-		booleanParam {			
-			name("SONAR_SCAN")
-			defaultValue(true)
-			description("Check to run SonarQube scan -> automatically runs tests for coverage report")
-		}
-	}
+		
+	    	booleanParam ( 			
+			name: 'RUN_TESTS',
+			defaultValue: 'true',
+			description: 'Check to run test'
+		),
+		booleanParam ( 			
+			name: 'SONAR_SCAN',
+			defaultValue: 'true',
+			description: 'Check to run SonarQube scan -> automatically runs tests for coverage report'
+		)        
+        
+    	])
+])	
 
     branchSources {
 
