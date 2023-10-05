6a388ef16174aa192780a95889ea9a142cfb3ae0
Arunkm039 <57870537+Arunkm039@users.noreply.github.com>
2023-10-05 13:41:53 +0530

    Update hotspot.groovy

--- hotspot.groovy
+++ hotspot.groovy
@@ -10,6 +10,42 @@ multibranchPipelineJob("hotspot/compute-engine/dev-qa/${jobName}") {
 
     displayName("${jobDisplayName} DEV QA Build")
 
+    parameters {
+		choiceParam {
+			name ("ENV")
+			description ("Choose target environment")
+			choices (['dev', 'qa', 'int'])
+		}
+
+		gitParameterDefinition {
+			name("GIT_BRANCH_TAG")
+			description("Available git branches")
+			type("PT_BRANCH_TAG")
+			defaultValue("")
+			branch("*")
+			branchFilter(".*")
+			tagFilter("*")
+			sortMode("DESCENDING_SMART")
+			selectedValue("DEFAULT")
+			useRepository("DevOpsTest.git")
+			quickFilterEnabled(true)
+			listSize("0")
+		}		
+
+		choiceParam {
+			name ("COMPONENT")
+			description("Choose component")
+			choices (['scheduler', 'worker', 'manager', 'model-worker'])
+		}
+
+		choiceParam {
+			name ("NAMESPACE")
+			description ("Choose namespace")
+			choices (['ctad-dev-amp-core', 'ctad-qa-amp-core', 'ctad-int-amp-core'])
+		}
+
+	}
+
     branchSources {
 
         github {
