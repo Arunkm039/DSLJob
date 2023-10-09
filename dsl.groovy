3ee8368f09f9135cdd97c28a59b53b480e0f77eb
Arunkm039 <57870537+Arunkm039@users.noreply.github.com>
2023-10-09 12:09:32 +0530

    Update dsl.groovy

--- dsl.groovy
+++ dsl.groovy
@@ -4,6 +4,19 @@ multibranchPipelineJob('my-multibranch-pipeline') {
             id('Github') // unique id
             remote('https://github.com/Arunkm039/DevOpsTest.git')
             credentialsId('https://github.com/Arunkm039/DevOpsTest.git') // Optional, if you have credentials
+
+            traits {
+                gitHubBranchDiscovery {
+                    strategyId(3) // Discover branches, but omit PRs
+                }
+                gitHubPullRequestDiscovery {
+                    strategyId(1) // Discover PRs and merge them with the current target branch
+                }
+                gitHubPullRequestFilter {
+                    excludeDraftRequest(true)
+                }
+                cron("H/2 * * * *") // Poll SCM every 2 minutes
+            }
         }
     }
     orphanedItemStrategy {
