0c99ad40d8fb79582b7ac38a335060818e639c3c
Arunkm039 <57870537+Arunkm039@users.noreply.github.com>
2023-10-14 21:31:59 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,27 +1,6 @@
 pipelineJob('hotspot/build-hotspot-bkservice') { 
 	displayName("Build Hotspot Services (JDK8)")
-	description("Hotspot Services builder pipeline")
-
-	parameters {
-		choiceParam {
-			name ("ENV")
-			description ("Choose target environment")
-			choices (['dev', 'qa', 'int'])
-		}		
-
-		choiceParam {
-			name ("COMPONENT")
-			description("Choose component")
-			choices (['scheduler', 'worker', 'manager', 'model-worker'])
-		}
-
-		choiceParam {
-			name ("NAMESPACE")
-			description ("Choose namespace")
-			choices (['ctad-dev-amp-core', 'ctad-qa-amp-core', 'ctad-int-amp-core'])
-		}
-
-	}
+	description("Hotspot Services builder pipeline")	
 
 	definition{
 		cpsScm {
@@ -33,7 +12,7 @@ pipelineJob('hotspot/build-hotspot-bkservice') {
 						url('https://github.com/Arunkm039/DevOpsTest.git')
 					}
 				}
-				scriptPath("Jenkinsfile")
+				scriptPath("Jenkinsfile.scale")
 			}	
 		}  	
 	}
