715cefdd1796e474fb08cb31ebc11b063b4f8150
Arunkm039 <57870537+Arunkm039@users.noreply.github.com>
2023-10-14 22:07:35 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,12 +3,13 @@ pipelineJob('hotspot/build-hotspot-bkservice') {
 	description("Hotspot Services builder pipeline")
 
 	parameters {
-		choiceParam {
-			name ("ENV")
-			description ("Choose target environment")
-			choices (['dev', 'qa', 'int'])
-		}
-	}	
+        choice(name: 'ENV', choices: ['dev', 'qa', 'int'], description: 'Choose target environment')
+        choice(name: 'NAMESPACE', choices: ['ctad-dev-amp-core', 'ctad-qa-amp-core', 'ctad-int-amp-core'], description: 'Choose namespace')
+        choice(name: 'ACTION', choices: ['Scale Up', 'Scale Down'], description: 'Choose scaling action')
+        // This choice will be dynamically populated later based on the chosen environment
+        choice(name: 'DEPLOYMENT', choices: [], description: 'Choose deployment to scale')
+        string(name: 'REPLICAS', defaultValue: '1', description: 'Replica count for scaling up')
+    }	
 
 	definition{
 		cpsScm {
